ec99e5f875fc994011faf700aa72f765122cbdde
Gustavo Gomes <gustsgomesbr@hotmail.com>
2021-03-11 09:24:43 -0400

    Lógica corrigida - projeto funcional!

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var quadrados = document.getElementsByClassName('quadrado')
 var jogador = 2;
 var jogadorInfo = document.getElementById('jogadorInfo')
-console.log(quadrados)
+
+
+var jogoAcabou = false
 
  //Define o bloco clicado e o jogador que clicou
 var blocos = {
@@ -20,6 +22,11 @@ var blocos = {
 
 
 function quadradoClick(id){
+    if(jogoAcabou == true){
+        window.location.reload()
+    }
+
+
     if(blocos[id][0] === 0){
     
         if(jogador === 1){
@@ -40,20 +47,66 @@ function quadradoClick(id){
         alert('Já jogou...')
     }
 
-    if(blocos[1][1] = blocos[2][1] = blocos[3][1]){
-        alert('HÁ UM VENCEDOR')
-    }
-    if(blocos[4][1] = blocos[5][1] = blocos[6][1]){
-        alert('HÁ UM VENCEDOR')
-    }
-    if(blocos[7][1] = blocos[8][1] = blocos[9][1]){
-        alert('HÁ UM VENCEDOR')
-    }
-    if(blocos[1][1] = blocos[5][1] = blocos[9][1]){
-        alert('HÁ UM VENCEDOR')
-    }
-    if(blocos[3][1] = blocos[5][1] = blocos[7][1]){
-        alert('HÁ UM VENCEDOR')
+    //VENCER NA HORIZONTAL
+
+    if(blocos[1][1] == 1 && blocos[2][1] == 1 && blocos[3][1] == 1){
+        anunciarVencedor(1)
+    }else if(blocos[1][1] == 2 && blocos[2][1] == 2 && blocos[3][1] == 2){
+        anunciarVencedor(2)
     }
 
+    if(blocos[4][1] == 1 && blocos[5][1] == 1 && blocos[6][1] == 1){
+        anunciarVencedor(1)
+    }else if(blocos[4][1] == 2 && blocos[5][1] == 2 && blocos[6][1] == 2){
+        anunciarVencedor(2)
+    }
+
+    if(blocos[7][1] == 1 && blocos[8][1] == 1 && blocos[9][1] == 1){
+        anunciarVencedor(1)
+    }else if(blocos[7][1] == 2 && blocos[8][1] == 2 && blocos[9][1] == 2){
+        anunciarVencedor(2)
+    }
+
+   //VENCER NA DIAGONAL
+   
+    if(blocos[1][1] == 1 && blocos[5][1] == 1 && blocos[9][1] == 1){
+        anunciarVencedor(1)
+    }else if(blocos[1][1] == 2 && blocos[5][1] == 2 && blocos[9][1] == 2){
+        anunciarVencedor(2)
+    }
+
+    if(blocos[3][1] == 1 && blocos[5][1] == 1 && blocos[7][1] == 1){
+        anunciarVencedor(1)
+    }else if(blocos[3][1] == 2 && blocos[5][1] == 2 && blocos[7][1] == 2){
+        anunciarVencedor(2)
+    }
+
+    //VENCER NA VERTICAL
+
+    if(blocos[1][1] == 1 && blocos[4][1] == 1 && blocos[7][1] == 1){
+        anunciarVencedor(1)
+    }else if(blocos[1][1] == 2 && blocos[4][1] == 2 && blocos[7][1] == 2){
+        anunciarVencedor(2)
+    }
+
+    if(blocos[2][1] == 1 && blocos[5][1] == 1 && blocos[8][1] == 1){
+        anunciarVencedor(1)
+    }else if(blocos[4][1] == 2 && blocos[5][1] == 2 && blocos[8][1] == 2){
+        anunciarVencedor(2)
+    }
+
+    if(blocos[3][1] == 1 && blocos[6][1] == 1 && blocos[9][1] == 1){
+        anunciarVencedor(1)
+    }else if(blocos[3][1] == 2 && blocos[6][1] == 2 && blocos[9][1] == 2){
+        anunciarVencedor(2)
+    }
+
+}
+
+//função para anunciar vencedor e resetar o game
+
+function anunciarVencedor(vencedor){
+    var gameStatus = document.getElementById('textGameStatus');
+    gameStatus.innerHTML = 'Jogador ' + vencedor + ' é o vencedor!!!'
+    jogoAcabou = true;
 }
